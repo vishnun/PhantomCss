796daeeae19496f1341661c151422ccf88342438
James Cryer <james.cryer@huddle.com>
2014-10-08 11:00:36 +0100

    Fixed result path capture.  Added NPM dependencies and gitignore

--- phantomcss.js
+++ phantomcss.js
@@ -161,7 +161,8 @@ function capture(srcPath, resultPath, target){
 			}
 
 			if(srcPath !== resultPath){
-				copyAndReplaceFile(srcPath, resultPath);
+				// can't use copyAndReplaceFile yet, so just capture again
+				casper.captureSelector(resultPath, target);
 			}
 
 			_onNewImage({
